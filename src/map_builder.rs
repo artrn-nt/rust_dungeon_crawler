62f02eff6d2051434455f96abfb84b1c0f93147d
Arthur Renon <renon.arthur@yahoo.fr>
2022-01-25 22:25:57 +0100

    Implemented a way to win the game

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -21,6 +21,26 @@ impl MapBuilder {
         mb.build_random_rooms(rng);
         mb.build_corridors(rng);
         mb.player_start = mb.rooms[0].center();
+
+        let dijkstra_map = DijkstraMap::new(
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            &vec![mb.map.point2d_to_index(mb.player_start)],
+            &mb.map,
+            1024.0
+        );
+
+        const UNREACHABLE: &f32 = &f32::MAX;
+        mb.amulet_start = mb.map.index_to_point2d
+            (
+                dijkstra_map.map
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, dist)| *dist < UNREACHABLE)
+                    .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+                    .unwrap().0
+            );
+
         mb
     }
 
