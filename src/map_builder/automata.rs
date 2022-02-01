12c5c3570c48ef1d56a68e49489d9f5f5812db03
Arthur Renon <renon.arthur@yahoo.fr>
2022-02-01 22:38:21 +0100

    Integrated steps in the map builder

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -13,6 +13,15 @@ impl MapArchitect for CellularAutomataArchitect {
             amulet_start: Point::zero(),
         };
 
+        self.random_noise_map(rng, &mut mb.map);
+        for _ in 0 .. 10 {
+            self.iteration(&mut mb.map);
+        }
+        let start = self.find_start(&mb.map);
+        mb.monster_spawns = mb.spawn_monsters(&start, rng);
+        mb.player_start = start;
+        mb.amulet_start = mb.find_most_distant();
+
         return mb
     }
 }
