26b692acb70cbb7976e6f7e56759130a3a3b4bf7
Arthur Renon <renon.arthur@yahoo.fr>
2022-02-03 01:09:08 +0100

    Implemented forest theme

--- src/map_builder/theme.rs
+++ src/map_builder/theme.rs
@@ -1,8 +1,15 @@
 use crate::prelude::*;
 
 pub struct DungeonTheme {};
+pub struct ForestTheme {};
 
 impl DungeonTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self {})
+    }
+}
+
+impl ForestTheme {
     pub fn new() -> Box<dyn MapTheme> {
         Box::new(Self {})
     }
@@ -13,6 +20,15 @@ impl MapTheme for DungeonTheme {
         match tile_type {
             TileType::Floor => to_cp437('.'),
             TileType::Wall => to_cp437('#')
+        }
+    }
+}
+
+impl MapTheme for ForestTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Floor => to_cp437(';'),
+            TileType::Wall => to_cp437('"'),
         }
     }
 }
